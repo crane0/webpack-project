6ade275ef88657155c4905dca39c88d48af3d2d0
crane0 <bhad5683126@163.com>
2019-12-22 01:17:17 +0800

    13tree shaking

--- src/search/index.js
+++ src/search/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom'
 import './search.less'
 import logo from './assets/author-icon.png'
 import { commonHello } from '../../utils/index.js'
+import { a } from './tree-shaking'
 
 class Search extends React.Component {
   render() {
     commonHello()
+    // a 这里虽然调用了，但执行效果没有被用到，所以tree-shaking就会将其擦除 。
+    const result = a()
     return (
       <div className="search-txt">
         search1 text 111
