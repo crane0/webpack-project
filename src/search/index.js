c6283a5990a0810425352b8fcf4fca3f5b36f6e6
crane0 <bhad5683126@163.com>
2019-12-23 18:51:12 +0800

    15代码分割和动态import

--- src/search/index.js
+++ src/search/index.js
@@ -2,18 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './search.less'
 import logo from './assets/author-icon.png'
-import { commonHello } from '../../utils/index.js'
-import { a } from './tree-shaking'
 
 class Search extends React.Component {
+  constructor() {
+    super(...arguments)
+    this.state = {
+      Text: null
+    }
+  }
+
+  // 动态引入
+  loadComponent() {
+    import('./test').then((Text) => {
+      this.setState({
+        Text: Text.default()
+      })
+    })
+  }
   render() {
-    commonHello()
-    // a 这里虽然调用了，但执行效果没有被用到，所以tree-shaking就会将其擦除 。
-    const result = a()
+    const { Text } = this.state
     return (
       <div className="search-txt">
+        {
+          Text ? Text : null
+        }
         search1 text 111
-        <img src={ logo } alt=""/>
+        <img src={ logo } alt="" onClick={this.loadComponent.bind(this)}/>
       </div>
     )
   }
